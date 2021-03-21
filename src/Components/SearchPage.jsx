6c6d4a717b88ab4b92e98451a1b7da6ff9554faf
kenmckaba <ken@mckaba.com>
2021-03-21 16:43:11 -0700

    added color themes & toggle

--- src/Components/SearchPage.jsx
+++ src/Components/SearchPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { func, number } from 'prop-types'
 import './SearchPage.css'
+import { Button, useColorMode } from '@chakra-ui/react'
 
 const SearchPage = ({ startPrice, endPrice, onSubmit }) => {
   const [startPriceLocal, setStartPriceLocal] = useState(startPrice)
@@ -10,9 +11,14 @@ const SearchPage = ({ startPrice, endPrice, onSubmit }) => {
     onSubmit(startPriceLocal, endPriceLocal)
   }
 
+  const { colorMode, toggleColorMode } = useColorMode()
+
   return (
     <div className="SearchPage">
-      <h1>Car Gurus AutoList Car Finder</h1>
+      <h1>Car Gurus AutoList Car Finder {colorMode}</h1>
+      <Button onClick={toggleColorMode}>
+        Toggle {colorMode === 'light' ? 'Dark' : 'Light'}
+      </Button>
       <h2>Enter a price range to search</h2>
       <div className="inputs">
         <div>
--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
+import theme from './theme'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
