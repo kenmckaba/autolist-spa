e657274747e7e84f7d4a1a01405c1ed4060982af
kenmckaba <ken@mckaba.com>
2021-03-21 17:52:43 -0700

    layout SearchPage

--- src/Components/SearchPage.jsx
+++ src/Components/SearchPage.jsx
@@ -1,7 +1,15 @@
+/* eslint-disable no-unused-vars */
 import React, { useState } from 'react'
 import { func, number } from 'prop-types'
-import './SearchPage.css'
-import { Button, useColorMode } from '@chakra-ui/react'
+import {
+  Button,
+  Heading,
+  HStack,
+  Input,
+  Stack,
+  useColorMode,
+  VStack,
+} from '@chakra-ui/react'
 
 const SearchPage = ({ startPrice, endPrice, onSubmit }) => {
   const [startPriceLocal, setStartPriceLocal] = useState(startPrice)
@@ -14,34 +22,29 @@ const SearchPage = ({ startPrice, endPrice, onSubmit }) => {
   const { colorMode, toggleColorMode } = useColorMode()
 
   return (
-    <div className="SearchPage">
-      <h1>Car Gurus AutoList Car Finder {colorMode}</h1>
+    <VStack spacing={5}>
+      <Heading size="xl">Car Gurus AutoList Car Finder</Heading>
       <Button onClick={toggleColorMode}>
         Toggle {colorMode === 'light' ? 'Dark' : 'Light'}
       </Button>
-      <h2>Enter a price range to search</h2>
-      <div className="inputs">
-        <div>
-          <input
-            value={startPriceLocal}
-            onChange={(e) => setStartPriceLocal(Number(e.target.value))}
-            type="text"
-            placeholder="Start range"
-          />
-        </div>
-        <div>
-          <input
-            value={endPrice}
-            onChange={(e) => setEndPriceLocal(Number(e.target.value))}
-            type="text"
-            placeholder="End range"
-          />
-        </div>
-      </div>
-      <button type="submit" onClick={submit}>
+      <Heading size="md">Enter a price range to search</Heading>
+      <HStack>
+        <Input
+          onChange={(e) => setStartPriceLocal(Number(e.target.value))}
+          type="text"
+          placeholder="Start range"
+        />
+        <Input
+          value={endPrice}
+          onChange={(e) => setEndPriceLocal(Number(e.target.value))}
+          type="text"
+          placeholder="End range"
+        />
+      </HStack>
+      <Button mt={5} onClick={submit}>
         Search
-      </button>
-    </div>
+      </Button>
+    </VStack>
   )
 }
 
